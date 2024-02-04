5250a48d1178fb54c197fef8c929bef8169c970d
Gideon-Yebei <0025.2022@students.mnu.ac.ke>
2024-02-04 05:42:41 +0300

    initial commit

---
+++ _printf.c
@@ -0,0 +1,5 @@
+//
+// Created by zenith on 2/3/24.
+//
+
+#include "_printf.h"
---
+++ _printf.h
@@ -0,0 +1,8 @@
+//
+// Created by zenith on 2/3/24.
+//
+
+#ifndef PRINTF__PRINTF_H
+#define PRINTF__PRINTF_H
+
+#endif //PRINTF__PRINTF_H
